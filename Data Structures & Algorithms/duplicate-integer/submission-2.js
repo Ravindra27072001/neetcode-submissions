c198327d56f26915f0091a478022136501585780
Ravindra Singh Rathore <91657705+Ravindra27072001@users.noreply.github.com>
2026-08-23 13:22:02 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        let set = new Set();
+        for(let i=0; i<nums.length; i++){
+            if(set.has(nums[i])){
+                return true;
+            }
+            set.add(nums[i])
+        }
+        return false;
+
+
+
+
+
+
+
+        // for (let i = 0; i < nums.length - 1; i++) {
+        //     for (let j = i + 1; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+    }
+}
